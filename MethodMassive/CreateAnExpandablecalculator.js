5e08ce9fff234d90643d7f82688b73da3810836d
lordb <bladov123@gmail.com>
2021-11-27 18:18:24 +0300

    test commit

---
+++ MethodMassive/CreateAnExpandablecalculator.js
@@ -0,0 +1,23 @@
+// function Calculator(){
+//     this.calculate = function(str){
+//         let arr = str.split(' ');
+//         if ( arr[1] == "+") {
+//             return +arr[0] + +arr[2];
+//         } else {
+//             return +arr[0] - +arr[2];
+//         }
+//     }
+// }
+
+// let rer = ['1', '+', '2'];
+//
+// function cac(arr) {
+//     if ( arr[1] == "+") {
+//         return +arr[0] + +arr[2];
+//     } else {
+//         return +arr[0] - +arr[2];
+//     }
+// }
+// console.log(cac(rer));
+
+console.log(1 + 2);
